4e04af0eb0845ae6dc53ee84c961f01961cb0abf
boticelli <kandelakitina@gmail.com>
2025-06-30 20:52:58 +0300

    Done my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,19 @@
+# frozen_string_literal: true
+
 module Enumerable
-  # Your code goes here
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  include Enumerable
+
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < size
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
 end
