83cb38cfe2662bff4518e74351676a9b3f8515db
boticelli <kandelakitina@gmail.com>
2025-07-02 10:05:21 +0300

    Add my all, my any, my count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
 module Enumerable
+  def my_all?(&block)
+    my_each do |element|
+      return false unless block.call(element)
+    end
+    true
+  end
+
+  def my_any?(&block)
+    my_each do |element|
+      return true if block.call(element)
+    end
+    false
+  end
+
+  def my_count(&block)
+    return size unless block_given?
+
+    count = 0
+    my_each do |element|
+      count += 1 if block.call(element)
+    end
+
+    count
+  end
 end
 
 class Array
